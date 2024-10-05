a8fe7073691a3b497e5d0b517855a223c824b093
Henry-Haley <henryeh2o@gmail.com>
2024-10-05 17:09:25 -0400

    Actually wrote the program. Only have it scan 1 port right now.

--- Projects/Code/Scanner.py
+++ Projects/Code/Scanner.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import socket
+
+def scanner(ip, port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    print(f"Scanning...")
+    print(f"Port {port} is open!")
+
+
+if __name__ == "__main__":
+    ip = input("What IP Address do you want to scan?")
+    #Only one port for now as I test
+    port = int(input("Enter the port"))
+    scanner(ip, port)
+
+
