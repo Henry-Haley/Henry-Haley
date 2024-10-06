d4298097e73b2e1fb682cc6c0a04ea35fe428ecf
Henry-Haley <henryeh2o@gmail.com>
2024-10-06 19:41:56 -0400

    Added the ability to iterate through a list given the start end end ports

--- Projects/Code/Scanner.py
+++ Projects/Code/Scanner.py
@@ -1,17 +1,21 @@
 import socket
 
-def scanner(ip, port):
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, port))
-    print(f"Scanning...")
-    print(f"Port {port} is open!")
+def scanner(ip, start_port, end_port):
+    for port in range(start_port, end_port + 1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(3)
+        result = s.connect_ex((ip, port))
+        if result == 0:
+            print(f"Port {port} is open")
+        else:
+            print(f"Port {port} is closed")
+        s.close()
 
 
 if __name__ == "__main__":
-    ip = input("What IP Address do you want to scan?")
-    #Only one port for now as I test
-    port = int(input("Enter the port"))
-    scanner(ip, port)
+    ip = input("What IP Address do you want to scan?\n")
+    start_port = int(input("Enter your starting port\n"))
+    end_port = int(input("Enter your last port you wish to scan\n"))
+    scanner(ip, start_port, end_port)
 
 
