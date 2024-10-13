7af67ccd55807e8664f7e7d0c4b9cef1ca0229c4
root <root@kali>
2024-10-13 15:47:13 -0400

    Quite a bit changed here. Added comments, Docstrings, made the code safer with Try/Except, cleaned it up a bit with a menu and some defined statements, and allows the use of Enter to scan common ports 0-1024

--- Projects/Code/Scanner.py
+++ Projects/Code/Scanner.py
@@ -1,8 +1,16 @@
 import socket
 
 def scanner(ip, start_port, end_port):
+    '''
+    Scans a range of user-supplied ports on a Specified IP Address
+
+    Parameters:
+        ip(str) : The IP address to scan
+        start_port (int): First port to scan
+        end_port (int): Last port to scan
+    '''
     for port in range(start_port, end_port + 1):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates IPv4 Socket
         s.settimeout(3)
         result = s.connect_ex((ip, port))
         if result == 0:
@@ -11,11 +19,41 @@ def scanner(ip, start_port, end_port):
             print(f"Port {port} is closed")
         s.close()
 
+# Menu function. Allows multiple inputs, like Enter for common ports. Planning as functionality grows to add a "man" page here
 
-if __name__ == "__main__":
-    ip = input("What IP Address do you want to scan?\n")
-    start_port = int(input("Enter your starting port\n"))
-    end_port = int(input("Enter your last port you wish to scan\n"))
+def display_menu(ip):
+    '''
+    Menu Function.
+
+    Parameters:
+        ip (str): The IP Address to scan
+    '''
+    port = input("Type your starting port, or hit Enter to scan common ports\n").strip()
+    if port == "":
+        start_port = 0
+        end_port = 1024
+    else:
+        try:
+            start_port = int(port)
+            end_port = int(input("Enter your end port\n"))
+        except ValueError:
+            print("Invalid input. Please enter numeric port values.")
+            return # Exits function if port number is invalid
+
+    # Calls teh scanner function
     scanner(ip, start_port, end_port)
 
+def main():
+    '''
+    Main function
+    '''
+    ip = input("What IP Address do you wish to scan?\n")
+    while True:
+        display_menu(ip)
+        again = input("Press 'Y' to scan again, or anything else to exit\n") #allows breaking of Loop
+        if again != "y":
+            print("GTFO!")
+            break
 
+if __name__ == "__main__":
+    main()
